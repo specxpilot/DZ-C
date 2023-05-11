5264b9e7d14f04b3b0f52357082748e6f7f05d3f
Дмитрий <specx@bk.ru>
2023-05-11 15:31:24 +0300

    ДЗ 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+ if(num1>num2)
+ {
+Console.WriteLine($"{num1} Это число больше");
+ } else {
+  Console.WriteLine($"{num2} Это число больше");
+ }
+ 
